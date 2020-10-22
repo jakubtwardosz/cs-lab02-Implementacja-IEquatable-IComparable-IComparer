44dda54e07bbc5583fd50fe24cb95a8859ae18e3
Jakub Twardosz <jtwardosz13@gmail.com>
2020-10-22 22:24:19 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cs_lab02_Implementacja_IEquatable_IComparable_IComparer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
