b380cc351a188e3575a30aa30f400fcdaf24561e
Jakub Twardosz <jtwardosz13@gmail.com>
2020-10-30 09:45:36 +0100

    task 1,2,3 is done

--- Program.cs
+++ Program.cs
@@ -6,7 +6,11 @@ namespace cs_lab02_Implementacja_IEquatable_IComparable_IComparer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var p1 = new Pracownik();
+            p1.Wynagrodzenie = 100;
+            p1.Nazwisko = "         Twardosz        ";
+            p1.DataZatrudnienia = new DateTime (2021, 10, 01);
+            Console.WriteLine(p1);
         }
     }
 }
---
+++ pracownik.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace cs_lab02_Implementacja_IEquatable_IComparable_IComparer
+{
+    public class Pracownik
+    {
+        private string _naz;
+        public string Nazwisko 
+        {
+            get => _naz;
+            
+            set => _naz = value.Trim();
+        }
+        private DateTime _dat = DateTime.Today;
+        public DateTime DataZatrudnienia 
+        {
+            get => _dat;
+            set => _dat = (value > DateTime.Today)? throw new ArgumentException("Zła data!") : value;
+        }      
+        
+        private decimal _wyn;
+        public decimal Wynagrodzenie
+        {
+            get => _wyn;
+            set => _wyn = (value < 0)? 0: value;
+        }
+
+        public override string ToString() => $"({Nazwisko}, {DataZatrudnienia:d MMM yyyy}, {Wynagrodzenie} PLN)";
+
+    }
+}
