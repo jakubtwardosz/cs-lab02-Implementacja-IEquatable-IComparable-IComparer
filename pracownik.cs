6b95ee400345cde448b0473a1efaed9c8a47f62b
Jakub Twardosz <jtwardosz13@gmail.com>
2020-11-07 17:43:53 +0100

    Ad. 7,8,9 done

--- pracownik.cs
+++ pracownik.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace cs_lab02_Implementacja_IEquatable_IComparable_IComparer
 {
-    public class Pracownik
+    public class Pracownik : IEquatable<Pracownik>
     {
         private string _naz;
         public string Nazwisko 
@@ -31,11 +31,44 @@ namespace cs_lab02_Implementacja_IEquatable_IComparable_IComparer
             DataZatrudnienia = DateTime.Today;
             Wynagrodzenie = 0;
         }
-
-        public override string ToString() => $"({Nazwisko}, {DataZatrudnienia:d MMM yyyy} ({CzasZatrudnienia}), {Wynagrodzenie} PLN)";
-
         public int CzasZatrudnienia => (DateTime.Now - DataZatrudnienia).Days / 30;
         
+        public override string ToString() => $"({Nazwisko}, {DataZatrudnienia:d MMM yyyy} ({CzasZatrudnienia} msc), {Wynagrodzenie} PLN)";
+        
+        public bool Equals(Pracownik other)
+        {
+            if (other is null) return false;
+
+            if (Object.ReferenceEquals(this, other))
+                return true;
+
+            return (Nazwisko == other.Nazwisko && DataZatrudnienia == other.DataZatrudnienia && Wynagrodzenie == other.Wynagrodzenie);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Pracownik)
+            {
+                return Equals((Pracownik)obj);    
+            }else{
+                return false;
+            }            
+        }
+
+        public override int GetHashCode() => (Nazwisko, DataZatrudnienia, Wynagrodzenie).GetHashCode();
+
+        public static bool Equals(Pracownik p1, Pracownik p2)
+        {
+            if ((p1 is null) && (p2 is null)) return true;
+
+            if ((p1 is null)) return false;
+
+            return p1.Equals(p2);
+        }
+
+        public static bool operator ==(Pracownik p1, Pracownik p2) => Equals(p1, p2);
+        public static bool operator !=(Pracownik p1, Pracownik p2) => !(p1 == p2);
 
     }
+
 }
